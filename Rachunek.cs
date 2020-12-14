a1f48c70d499d4ab1b65eaeace00493cf7d4278a
Floxbiee <pwilkosz99@gmail.com>
2020-12-14 09:35:56 +0100

    0.11

---
+++ Rachunek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecepcjaWHotelu
+{
+    class Rachunek
+    {
+        protected int RachunekID;
+        int Wielkosc;
+        //Date TerminUregulowania;
+        bool CzyUregulowano;
+        //Zakwaterowanie ZaZakwaterowanie;
+        public void Zaksieguj()
+        {
+
+        }
+        public void CzyUregulowany()
+        {
+
+        }
+    }
+}
---
+++ Rezerwacja.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecepcjaWHotelu
+{
+    class Rezerwacja
+    {
+        protected int RezerwacjaID;
+        int klientID;
+        //Date DataOd;
+        //Date DataDo;
+        int ileOsob;
+        public void WprowadzDane()
+        {
+
+        }
+        public void AnulowanieRezerwacji()
+        {
+
+        }
+        public void WprowadzDodatkoweOpcje()
+        {
+
+        }
+    }
+}
